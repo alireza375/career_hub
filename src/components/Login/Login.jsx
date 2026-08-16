fc7fff1808212ea740fe4bdd344998be2fe8e1b9
alireza375 <alirezaraju8@gmail.com>
2026-08-16 17:07:50 +0600

    login form design properly

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,55 @@
+import { useState } from "react";
 import { NavLink } from "react-router";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login</h2>
-                <NavLink to="/registration" className="btn">Registration</NavLink>
 
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleLogin = e => {
+        e.preventDefault();
+        console.log('Form Submitted');
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log(email, password);
+    }
+
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card-body">
+                    <form onSubmit={handleLogin}>
+                            <fieldset className="fieldset">
+                            <label className="label">Email</label>
+                            <input type="email" className="input" placeholder="Enter Email" name="email" required />
+                            <label className="label">Password</label>
+                            <input 
+                                type={ showPassword ? "text" : "password" } 
+                                className="input" 
+                                placeholder="Enter Password" 
+                                name="password" 
+                                required /> <span onClick={ () => setShowPassword(!showPassword)}>
+                                    {
+                                        showPassword ? <FaEyeSlash className="text-xl" /> : <FaEye className="text-xl" />
+                                    }
+                                    </span>
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-neutral mt-4">Login</button>
+                            <button className="btn btn-neutral mt-1"><NavLink to='/registration'>Sign Up</NavLink></button>
+                            </fieldset>
+                    </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,45 @@
+import { NavLink } from "react-router";
 
 const Register = () => {
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        console.log('Form Submitted');
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log(name, email, password);
+    }
+    
+    
+
     return (
-        <div>
-            <h2>Registration</h2>
+      <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Registration Form!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card-body">
+                    <form onSubmit={handleRegister}>
+                            <fieldset className="fieldset">
+                            <label className="label">Name</label>
+                            <input type="text" className="input" placeholder="Enter Name" name="name" />
+                            <label className="label">Email</label>
+                            <input type="email" className="input" placeholder="Enter Email" name="email" />
+                            <label className="label">Password</label>
+                            <input type="password" className="input" placeholder="Enter Password" name="password" />
+                            <button className="btn btn-neutral mt-1">Sign Up</button>
+                            </fieldset>
+                    </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
